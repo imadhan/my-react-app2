854ab63dcba29ecfc56dcb2b7210236a1b34cce8
madhanmadhan <madhankumarjn3@gmail.com>
2020-02-16 20:30:25 +0530

    added data.json file and created table with alert

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data.json';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My bio</h1>
+        
+        <table id='students'>
+        
+               <tbody>
+                  
+                  <tr><td>Name</td><td>{data.name}</td></tr>
+                  <tr><td>Age</td><td>{data.age}</td></tr>
+                  <tr><td>Gender</td><td>{data.gender}</td></tr>
+                  <tr><td>Hobby</td><td>{data.hobby}</td></tr>
+                  <tr><td>Experience</td><td>{data.experience}</td></tr>
+               
+               </tbody>
+            </table>
+            <button onClick={() =>hello(data.name)}>Click</button>
     </div>
   );
+  
+}
+const hello = name => {
+  alert(name);
 }
 
 export default App;
